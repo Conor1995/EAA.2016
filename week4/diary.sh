5f08232cf63303753b82f25c1e9410dbd0ab4f43
John Burns <jzburns@gmail.com>
2016-10-13 10:42:44 +0100

    Diary.sh changed to mydiary.txt

--- week4/diary.sh
+++ week4/diary.sh
@@ -1,9 +1,10 @@
 #!bin/bash/
 
 read $var
-echo `date +%D-%r` $var >> diary.sh 
+echo `date +%D-%r` $var >> mydiary.txt
 
 
+#LAB4
 
 #[eaauser@localhost ~]$ read var
 #hello EAA users
@@ -17,4 +18,11 @@ echo `date +%D-%r` $var >> diary.sh
 #[eaauser@localhost ~]$ echo `date +%D-%r` $var
 #10/13/16-09:52:43 hello EAA users
 #[eaauser@localhost ~]$ 
+
+
+#Pushing something to github
+
+#[eaauser@localhost week4]$ git add diary.sh
+#[eaauser@localhost week4]$ git commit
+#[eaauser@localhost week4]$ git push
 
